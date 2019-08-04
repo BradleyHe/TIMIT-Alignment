13b6b4fb10a10621241f8ecf9d0331a58f6287cc
Bradley H <bhe6001@gmail.com>
2019-08-04 17:33:21 -0400

    Fixed error with timit alignment

--- align_timit.py
+++ align_timit.py
@@ -7,20 +7,13 @@ source_path = os.path.join(bin_path,  'TIMIT', 'TEST')
 # generate .lab files
 for dirName, subdirList, fileList in os.walk(source_path):
 	for file in fileList:
-		# comment out if you want to keep these files, but they are not needed for preprocessing
-		if file.endswith('.WAV') or file.endswith('WRD'):
-			os.remove(dirName + '/' + file)
-			continue
-
-		if file.endswith('wav'):
-			continue
-
-		# files remaining are .TXT
-		# rename to .lab and edit to correct format
-		with open(dirName + '/' + file) as f:
-			str = ''.join(x for x in f.read() if not x.isdigit() and not x == '.')
-			with open(dirName + '/' + file[:-4] + '.lab', 'w+') as w:
-				w.write(str.lstrip())
+		if file.endswith('.TXT'):
+			# files remaining are .TXT
+			# rename to .lab and edit to correct format
+			with open(dirName + '/' + file) as f:
+				str = ''.join(x for x in f.read() if not x.isdigit() and not x == '.')
+				with open(dirName + '/' + file[:-4] + '.lab', 'w+') as w:
+					w.write(str.lstrip())
 		
 # run montreal forced aligner on all voice files
 # will run only in test files
